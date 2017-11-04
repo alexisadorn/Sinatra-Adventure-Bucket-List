8143b54261e9a651217530fea0f387de58ef7128
alexisadorn <alexisadorn@gmail.com>
2017-11-04 15:04:43 -0500

    Creating new experience & categories in new experience route

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -11,7 +11,37 @@ class ExperiencesController < ApplicationController
   end
 
   post '/experiences' do
-    binding.pry
+    details = params["experience"]
+    category_name = params["category"]["name"]
+    category_ids = params["category"]["category_ids"]
+
+    details.each do |att, val|
+      if val.empty?
+        flash[:empty] = "Please complete all fields!"
+        redirect to '/experiences/new'
+      end
+    end
+
+    exp = Experience.new(details)
+    exp.user_id = session[:user_id]
+    unless category_name.empty?
+      if Category.find_by(:name => category_name) #check for duplicates
+        category = Category.find_by(:name => category_name)
+      else
+        category = Category.create(:name => category_name)
+      end
+      exp.categories << category
+    end
+    if category_ids
+      category_ids.each do |id|
+        exp.categories << Category.find(id)
+      end
+    end
+
+    exp.save
+
+    flash[:success] = "Successfully created new experience!"
+    redirect to "experiences/#{exp.id}"
   end
 
   get '/experiences/:id' do
